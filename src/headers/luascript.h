a2f8731da6424cc7563b7847614dfdaa839687a0
RoranBlackfire <memotoskbhat@gmail.com>
2020-06-25 00:55:20 +0530

    cannot make getKeys work Why isn't lua working properly...

--- src/headers/luascript.h
+++ src/headers/luascript.h
@@ -80,6 +80,13 @@ public:
     template<class T>
     T lua_getDefault();
 
+    void clean();
+
+    template<class T>
+    std::vector<T> getVector(const std::string &name);
+
+    std::vector<std::string> getTableKeys(const std::string &name);
+
 private:
     lua_State* L;
 };
@@ -141,6 +148,97 @@ inline std::string LuaScript::lua_get(const std::string &varname){
         printError(varname, "Not a string");
     }
     return s;
+}
+
+void LuaScript::clean(){
+    int n = lua_gettop(L);
+    lua_pop(L, n);
+}
+
+template<>
+std::vector<int> LuaScript::getVector(const std::string &name){
+    std::vector<int> v;
+    lua_getglobal(L, name.c_str());
+    if(lua_isnil(L, -1)){
+        return std::vector<int>();
+    }
+    
+    lua_pushnil(L);
+    
+    while(lua_next(L, -2)){
+        v.push_back((int)lua_tonumber(L, -1));
+        lua_pop(L, 1);
+    }
+    clean();
+    return v;
+}
+
+template<>
+std::vector<std::string> LuaScript::getVector(const std::string &name){
+    std::vector<std::string> v;
+    lua_getglobal(L, name.c_str());
+    if(lua_isnil(L, -1)){
+        return  std::vector<std::string>();
+    }
+    
+    lua_pushnil(L);
+    
+    while(lua_next(L, -2)){
+        v.push_back(std::string(lua_tostring(L, -1)));
+        lua_pop(L, 1);
+    }
+    clean();
+    return v;
+}
+
+std::vector<std::string> LuaScript::getTableKeys(const std::string &name){
+    //hullo
+    // std::string keyfunc = 
+    //     "function getKeys(name)\n"
+    //     "   s = \"\" \n"
+    //     "   for k, v in pairs(_G[name]) do\n"
+    //     "       s = s..k..\",\" \n"
+    //     "       end \n"
+    //     "   return s \n"
+    //     "end \n";
+    
+    // if(0 != luaL_loadstring(L, keyfunc.c_str())){
+    //     std::cout << "no function created\n";
+    //     return std::vector<std::string> ();
+    // }
+    // std::cout << keyfunc.c_str();
+    // lua_pcall(L, 0, 0, 0);// a way of appending to the function i think
+    lua_getglobal(L, name.c_str());
+    if(lua_isnil(L, -1)){
+        std::cout << "cannot load nil value\n";
+        return std::vector<std::string>();
+    }
+
+    clean();
+
+    lua_getglobal(L, "getKeys");
+    if(lua_isnil(L, -1)){
+        std::cout << "this is not good\n";
+        return std::vector<std::string>();
+    }
+    lua_pushstring(L, name.c_str());
+    lua_pcall(L, 1, 1, 0);
+    //here comes the separation part
+    std::string test = lua_tostring(L, -1);
+    std::cout << test << '\n';
+    std::vector<std::string> keys;
+    std::string temp = "";
+
+    for (long unsigned int i = 0; i < test.size(); i++){
+        if(test.at(i) != ',') {
+            temp += test.at(i);
+        } else {
+            keys.push_back(temp);
+            temp= "";
+        }
+    }
+
+    return keys;
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -12,5 +12,23 @@ int main(){
     //std::cout << std::string(script.get("player.filename")) << '\n' << (int)script.get("player.pos.x");
     std::string file = script.get<std::string>("player.filename");
     int posx = script.get<int>("player.pos.x");
-    std::cout << file << '\n' << posx << '\n';
+    std::cout << file << '\n' << posx << '\n'; //check
+    std::vector<int> v = script.getVector<int>("array");
+    for (auto it = v.begin(); it != v.end(); ++it){
+        std::cout << *it << '\n';
+    }
+    //check
+
+    //this is to get all the keys of a table
+    std::vector<std::string> player_keys = script.getTableKeys("player");
+
+    for (auto it = player_keys.begin(); it != player_keys.end(); ++it){
+        std::cout << *it << '/n';
+    }
+
+    std::vector<std::string> v2 = script.getVector<std::string>("barry");
+    for (auto it = v2.begin(); it != v2.end(); ++it){
+        std::cout << *it << '\n';
+    }
+    //check
 }
