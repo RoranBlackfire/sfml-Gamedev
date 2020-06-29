a0ba8d1387748efef02460d92452582f358df658
RoranBlackfire <memotoskbhat@gmail.com>
2020-06-29 19:38:42 +0530

    made template window appearance

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,21 @@
 #include <lua.hpp>
 #include "LuaScript.h"
 
+//note: run test.cpp to see a good example
 
 int main(){
-    
+    std::cout << "hello SFML and ImGui\n";
+    sf::RenderWindow window(sf::VideoMode(640, 480), "Main Window");
+    window.setFramerateLimit(120);
+    sf::Clock clock;
+
+    //renders a blank window
+    while(window.isOpen()){
+        sf::Event event;
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed){
+                window.close();
+            }
+        }
+    }
 }
